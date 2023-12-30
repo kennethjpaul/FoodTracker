3b9ddb4db2fc82cef1cd84d858ec4488123f85da
Kenneth J Paul <kenneth.josephpaul@protonmail.com>
2023-12-30 04:05:35 +0100

    UI: Design of the modify food page

--- app/src/main/java/com/kinetx/foodtracker/viewmodel/ModifyFoodVM.kt
+++ app/src/main/java/com/kinetx/foodtracker/viewmodel/ModifyFoodVM.kt
@@ -1,8 +1,36 @@
 package com.kinetx.foodtracker.viewmodel
 
 import android.app.Application
+import android.view.View
 import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import com.kinetx.foodtracker.fragment.ModifyFoodFragmentArgs
 
 class ModifyFoodVM(application: Application, args: ModifyFoodFragmentArgs): AndroidViewModel(application) {
+
+    private val _isEditVisible = MutableLiveData<Int>()
+    val isEditVisible : LiveData<Int>
+        get() = _isEditVisible
+
+    private val _isCreateVisible = MutableLiveData<Int>()
+    val isCreateVisible : LiveData<Int>
+        get() = _isCreateVisible
+
+
+    init {
+        when(args.foodId)
+        {
+            -1L->
+            {
+                _isCreateVisible.value = View.VISIBLE
+                _isEditVisible.value = View.GONE
+            }
+            else->
+            {
+                _isCreateVisible.value = View.GONE
+                _isEditVisible.value = View.VISIBLE
+            }
+        }
+    }
 }
